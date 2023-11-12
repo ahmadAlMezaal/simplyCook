adffe46837789fcf2c5c7efd007d1a3df6101bb4
Ahmad Mazaal <Ahmad.hmazaal@gmail.com>
2023-11-12 12:50:48 +0000

    feat: add import alias + linting config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,40 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: 'react-native-dotenv',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        'root': ['.'],
+        'extensions': [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.tsx',
+          '.ios.tsx',
+          '.android.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        'alias': {
+          '#navigation': './src/navigation',
+          '#components': './src/components',
+          '#screens': './src/screens',
+          '#services': './src/services',
+          '#types': './src/types',
+          '#assets': './assets',
+          '#root': './',
+        },
+      },
+    ],
+  ],
 };
